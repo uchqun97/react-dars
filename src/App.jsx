eafb2a6d97d85e337accdbcdb72bd6db05c285b1
uchqun97 <uchqun1997shoh@gmail.com>
2024-11-30 15:02:13 +0500

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,8 @@
 export const App = () => {
-    const usersInfo = [
-        {
-            ism: "John",
-            email: "john@example.com",
-            shaxar: "United States"
-        },
-        {
-            ism: "Johan",
-            email: "johan@example.com",
-            shaxar: "Germany"
-        },
-        {
-            ism: "Alexa",
-            email: "alexa@example.com",
-            shaxar: "French"
-        },
-        {
-            ism:"Husan",
-            email: "husan@example.com",
-            shaxar: "Uzbekistan"
-        }
-    ]
     return (
-        <main>
-            {usersInfo.map(({ism,email,shaxar})=>(
-                <ol key={Math.random()}>
-                    <li>{ism}</li>
-                    <li>{email}</li>
-                    <li>{shaxar}</li>
-                </ol>
-            ))}
-        </main>
+        <div>
+            <h1 style={{color: "white", backgroundColor: "teal", padding: "2rem"}}>Inline Style</h1>
+        </div>
     )
 }
+
