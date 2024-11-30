e874d8bb57cda5f479d9f06bc70b606a8740df8b
uchqun97 <uchqun1997shoh@gmail.com>
2024-11-30 15:02:13 +0500

    Conditional Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
+import Salomlashish from "./components/Salomlashish.jsx";
 export const App = () => {
-    const usersInfo = [
-        {
-            ism: "John",
-            email: "john@example.com",
-            shaxar: "United States"
-        },
-        {
-            ism: "Johan",
-            email: "johan@example.com",
-            shaxar: "Germany"
-        },
-        {
-            ism: "Alexa",
-            email: "alexa@example.com",
-            shaxar: "French"
-        },
-        {
-            ism:"Husan",
-            email: "husan@example.com",
-            shaxar: "Uzbekistan"
-        }
-    ]
     return (
-        <main>
-            {usersInfo.map(({ism,email,shaxar})=>(
-                <ol key={Math.random()}>
-                    <li>{ism}</li>
-                    <li>{email}</li>
-                    <li>{shaxar}</li>
-                </ol>
-            ))}
-        </main>
+        <div>
+            {/*<Weather/>*/}
+            {/*<UserStatus loggedIn={true} isAdmin={false} />*/}
+            <Salomlashish timeOfDay="ertalab" />
+        </div>
     )
 }
+
